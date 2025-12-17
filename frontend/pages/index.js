f862dd5da6d95f36b4d96f1121a2a721abf5db68
Anagha Kumar <anaghaofficial01@gmail.com>
2025-12-17 13:24:56 +0530

    Updated the backend API url

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,11 +4,13 @@ export default function Home() {
   const [status, setStatus] = useState("");
 
   useEffect(() => {
-  fetch("http://127.0.0.1:8000/health")
-    .then(res => res.json())
-    .then(data => setStatus(data.status))
-    .catch(err => console.error("Backend fetch failed:", err));
-}, []);
+    const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/health`;
+
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => setStatus(data.status))
+      .catch(err => console.error("Backend fetch failed:", err));
+  }, []);
 
   return (
     <main style={{ padding: 24 }}>
